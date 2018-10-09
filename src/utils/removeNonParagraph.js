1f465b9a1648ed31f4159316a0a92caf27d766e4
tuanna <tuannguyenanh1412@gmail.com>
2018-10-10 01:01:48 +0700

    mot ngay dai

--- src/utils/removeNonParagraph.js
+++ src/utils/removeNonParagraph.js
@@ -1,14 +1,13 @@
-var inlineElements = ["b", "big", "i", "small", "tt", "abbr", "acronym", "cite", "code", "dfn", "em", "kbd", "strong",
-    "samp", "time", "var", "a", "bdo", "br", "img", "map", "object", "q", "script", "span", "sub", "sup", "button",
-    "input", "label", "select", "textarea", "u"];
-
-var inlineElementsStartRegex = new RegExp("<(" + inlineElements.join("|") + ")[^>]*?>", "gi");
-var inlineElementsEndRegex = new RegExp("</(" + inlineElements.join("|") + ")[^>]*?>", "gi");
-
-var stripInlineTagsAtStartEnd = function (text) {
-    text = text.replace(inlineElementsStartRegex, "");
-    text = text.replace(inlineElementsEndRegex, "");
+var blockElements = [ "address", "article", "aside", "blockquote", "canvas", "dd", "div", "dl", "fieldset", "figcaption",
+	"figure", "footer", "form", "header", "hgroup", "hr", "li", "main", "nav",
+	"noscript", "ol", "output", "pre", "section", "table", "tfoot", "ul", "video" ];
+var removeNonParagraph = function(text) {
+    var nonParagraphRegex = new RegExp('<('+ blockElements.join('|') + ')(?:[^>]+)?>.*?<\/\1>', "gi");
+    text = text.replace(nonParagraphRegex, "");
     return text;
-};
+}
 
-module.exports = stripInlineTagsAtStartEnd;
+var data = require('../example/paper.json');
+
+console.log(removeNonParagraph(data.content));
+
